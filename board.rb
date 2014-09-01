3fffd9d7f09937616ca8c116960130f33e2d41e5
Muaaz Saleem <muaazsaleem@outlook.com>
2014-09-02 01:33:05 +0500

    considered diagnols

--- board.rb
+++ board.rb
@@ -7,7 +7,7 @@ class Board
 		@squares = [[" "," "," "], [" "," "," "], [" "," "," "]] #squares[rows][columns]
 		@rows = []
 		@columns = []
-		@diagols = [] #diagnol[0] = [[0,0],[1,1],[2,2]] diagnol[1] = [[0,2],[1,1],[2,0]]
+		@diagnols = [] #diagnol[0] = [[0,0],[1,1],[2,2]] diagnol[1] = [[0,2],[1,1],[2,0]]
 		
 		3.times do |i|
 			3.times do |j|
@@ -15,12 +15,13 @@ class Board
 			end
 		end
 
-
-
 		3.times { |i| @rows[i] = Row.new }
 		3.times { |i| @columns[i] = Column.new}
-		2.times { |i| @diagols[i] = Diagnol.new }
+		2.times { |i| @diagnols[i] = Diagnol.new }
 		
+		@diagnols[0] = [@squares[0,0], @squares[1,1], @squares[2,2]]
+		@diagnols[1] = [@squares[0,2], @squares[1,1], @squares[2,0]]
+
 		3.times do |i|
 			3.times do |j|
 				@rows[i].squares[j] =  @squares[i][j] 
